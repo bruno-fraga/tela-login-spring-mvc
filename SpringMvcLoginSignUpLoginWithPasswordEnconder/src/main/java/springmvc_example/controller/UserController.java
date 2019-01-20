c1e73dfe20cea2a6c1001ea37b0a621cbaf0901c
Bruno Fraga <Bruno Fraga@BRUNO-FRAGA>
2019-01-20 12:54:06 -0200

    Mudar Senha

--- SpringMvcLoginSignUpLoginWithPasswordEnconder/src/main/java/springmvc_example/controller/UserController.java
+++ SpringMvcLoginSignUpLoginWithPasswordEnconder/src/main/java/springmvc_example/controller/UserController.java
@@ -2,6 +2,7 @@ package springmvc_example.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -23,4 +24,10 @@ public class UserController {
 		return model;
 	}
 
+	@RequestMapping(value = "/changePass{username}", method = RequestMethod.GET)
+	public ModelAndView changePass(@PathVariable("username") String username) {
+		ModelAndView model = new ModelAndView("user/change_pass");
+		model.addObject("user", userService.findUserByUsername(username));
+		return model;
+	}
 }
