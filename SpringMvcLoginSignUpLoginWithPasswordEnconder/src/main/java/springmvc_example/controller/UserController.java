cff720070ec7467cced9a6d4baa5ee52379e21b9
Bruno Fraga <Bruno Fraga@BRUNO-FRAGA>
2019-01-21 23:11:00 -0200

    Tela de Entrada

--- SpringMvcLoginSignUpLoginWithPasswordEnconder/src/main/java/springmvc_example/controller/UserController.java
+++ SpringMvcLoginSignUpLoginWithPasswordEnconder/src/main/java/springmvc_example/controller/UserController.java
@@ -1,16 +1,20 @@
 package springmvc_example.controller;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import springmvc_example.form.UserForm;
+import springmvc_example.model.UserInfo;
 import springmvc_example.service.UserService;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/user")
 public class UserController {
 
 	@Autowired
@@ -29,5 +33,38 @@ public class UserController {
 		ModelAndView model = new ModelAndView("user/change_pass");
 		model.addObject("user", userService.findUserByUsername(username));
 		return model;
+	}
+
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public ModelAndView save(@ModelAttribute("user") UserInfo user) {
+		ModelAndView model = new ModelAndView();
+
+		userService.update(user.getUsername(), user.getPassword());
+		model.addObject("msg", "Password change successful");
+
+		return model;
+
+	}
+
+	@RequestMapping(value = "/signup", method = RequestMethod.GET)
+	public ModelAndView signup() {
+		ModelAndView model = new ModelAndView();
+		model.addObject("userForm", new UserForm());
+		return model;
+	}
+
+	@RequestMapping(value = "/register", method = RequestMethod.POST)
+	public String register(@ModelAttribute("userForm") UserForm userForm, BindingResult result,
+			RedirectAttributes redirectAttributes) {
+
+		if (result.hasErrors()) {
+			return "/user/signup";
+		} else {
+			userService.add(userForm.getUsername(), userForm.getPassword());
+			redirectAttributes.addFlashAttribute("msg", "You Account has been created successfully");
+
+			return "redirect:/login";
+		}
+
 	}
 }
